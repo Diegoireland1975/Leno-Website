7289ec28245d8bd5972e0e0f58b0e19c2d928867
Diego Raffa <diego170975@gmail.com>
2025-08-23 14:47:57 +0100

    change hamburger icon to x on click with transition on rotation

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
+// *******************************
 // Mobile menu is showing on click
+// **************************
 const mobileToggle = document.querySelector(".navbar__mobile-menu-toggle");
 const mobileList = document.querySelector(".navbar__mobile-menu-list");
 
 mobileToggle.addEventListener("click", (e) => {
   mobileList.classList.toggle("active");
+
+  // ***********************************
+  /* transfrom hamburger icon into X */
+  // **********************************
+
+  mobileToggle.classList.toggle("active");
+  // select icon inside button
+  const icon = mobileToggle.querySelector("i");
+
+  // replace icon  that is currently displayed
+  if (icon.classList.contains("fa-bars")) {
+    icon.classList.replace("fa-bars", "fa-xmark"); // swap to X
+  } else {
+    icon.classList.replace("fa-xmark", "fa-bars"); // swap back
+  }
 });
