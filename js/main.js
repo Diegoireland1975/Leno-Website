49b77c8d66f9cc532204e3444c1060b9ec0793e5
Diego Raffa <diego170975@gmail.com>
2025-09-10 19:33:35 +0200

    adding class with js to change background color and adding blur on navbar

--- js/main.js
+++ js/main.js
@@ -22,3 +22,13 @@ mobileToggle.addEventListener("click", (e) => {
     icon.classList.replace("fa-xmark", "fa-bars"); // swap back
   }
 });
+// change background color and add blue on scroll
+const navbar = document.querySelector(".navbar");
+
+window.addEventListener("scroll", (e) => {
+  if (window.scrollY > 0) {
+    navbar.classList.add("navbar--scroll");
+  } else {
+    navbar.classList.remove("navbar--scroll");
+  }
+});
